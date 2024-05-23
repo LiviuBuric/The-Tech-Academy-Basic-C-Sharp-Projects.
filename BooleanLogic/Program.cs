1de3cbdbeaf43e14a1e60a19d0f4258710cba276
Liviu Buric <liviu_buric@yahoo.com>
2024-05-23 11:43:06 +0100

    booleanLogic added

---
+++ BooleanLogic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the base of the triangle:");
+            double baseValue = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the height of the triangle:");
+            double heightValue = Convert.ToDouble(Console.ReadLine());
+
+            double area = 0.5 * baseValue * heightValue;
+
+            Console.WriteLine("The area of the triangle is: " + area);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int age = 31;
+            bool hasTicket = true;
+            bool hasPassport = true;
+            bool canFly = (age >= 18 && hasTicket && hasPassport);
+            Console.WriteLine(canFly);
+            Console.ReadLine();
+        }
+    }
+}
