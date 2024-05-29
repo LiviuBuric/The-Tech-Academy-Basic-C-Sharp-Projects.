da685d0ffb5944078276d74038c8f2359b648ffa
Liviu Buric <liviu_buric@yahoo.com>
2024-05-29 13:15:50 +0100

    f

---
+++ NumberApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NumberApp
+{
+    // Define a struct called Number
+    public struct Number
+    {
+        // Property: Amount (of data type decimal)
+        public decimal Amount { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of data type Number and initialize it
+            Number myNumber = new Number();
+
+            // Assign an amount (e.g., 123.45) to the Amount property
+            myNumber.Amount = 123.45M; // M suffix indicates decimal literal
+
+            // Print the amount to the console
+            Console.WriteLine($"Amount: {myNumber.Amount}");
+        }
+    }
+}
