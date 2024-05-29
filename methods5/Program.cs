3e8c9577ade92a40171cd0eea9b07069428aa130
Liviu Buric <liviu_buric@yahoo.com>
2024-05-29 11:32:50 +0100

    g

---
+++ methods5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodsAndObjectsAssignment
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + this.FirstName + " " + this.LastName);
+        }
+    }
+
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sample = new Employee();
+            sample.FirstName = "Sample";
+            sample.LastName = "Student";
+
+            sample.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
