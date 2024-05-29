fba91b816e1940c8b46acde87f56cd9d35fe4b75
Liviu Buric <liviu_buric@yahoo.com>
2024-05-29 10:38:17 +0100

    f

---
+++ methods3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int results = MathOp(num1, num2);
+            Console.WriteLine(num1 + " + " + num2 + " = " + results);
+        }
+
+        public static int MathOp(int x, int y = 0)
+        {
+            return x + y;
+        }
+
+        public static int MathOp(int x)
+        {
+            return x * 10;
+        }
+    }
+}
