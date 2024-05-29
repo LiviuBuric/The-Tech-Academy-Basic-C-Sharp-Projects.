b29e6344682c2c64021e5a14bc4afeb4c03d1dc4
Liviu Buric <liviu_buric@yahoo.com>
2024-05-29 10:34:52 +0100

    g

---
+++ methods2/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    public class MathMethods2Cs
+    {
+        // First method that takes an integer
+        public int MathOp(int number)
+        {
+            return number + 7;
+        }
+
+        // Second method that takes a decimal (commented out)
+        // public int MathOp(decimal number)
+        // {
+        //     return newnum = Convert.ToInt32(number * 20);
+        // }
+
+        // Third method that takes a string
+        public int MathOp(string number)
+        {
+            int newnum = Convert.ToInt32(number);
+            return newnum * 3;
+        }
+    }
+}
---
+++ methods2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MainMethodAssign
+{
+    class Program
+    {
+        // static void Main(string[] args)
+        // {
+        //     // Instantiate the MathMethods class
+        //     MathMethods method1 = new MathMethods();
+        //     // Calls the first method with an int parameter
+        //     Console.WriteLine(method1.MathTop(7));
+        //     // Calls the second method (string) with a decimal parameter
+        //     Console.WriteLine(method1.MathTop(7.5m));
+        //     // Call the third method with a string parameter
+        //     Console.WriteLine(method1.MathTop("7"));
+        // }
+    }
+}
